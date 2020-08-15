de7c6aae52b3827564905c36f0b56acde8e1d413
Choyeon <Choyeon.engr@gmail.com>
2020-08-15 13:01:30 +0900

    Programmers: 더 맵게
    programmers.co.kr/learn/courses/30/lessons/42626

---
+++ MoreSpicy.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K)
+{
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for (int s : scoville)
+        pq.emplace(s);
+    
+    while (pq.top() < K)
+    {
+        if (pq.size() == 1) return -1;
+        
+        else
+        {
+            int fst = pq.top();
+            pq.pop();
+            
+            int snd = pq.top();
+            pq.pop();
+            
+            pq.emplace(fst + snd * 2);
+            
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
