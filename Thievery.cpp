70d292bafc565ecc045efda2ffd381518909e9ae
Choyeon <Choyeon.engr@gmail.com>
2020-10-03 15:12:23 +0900

    Programmers: 도둑질
    programmers.co.kr/learn/courses/30/lessons/42897

---
+++ Thievery.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> money)
+{
+    int memo1[1000000] = { 0 };
+    int memo2[1000000] = { 0 };
+    
+    memo1[0] = money[0];
+    memo1[1] = money[0];
+    for (int i = 2; i < money.size() - 1; ++i)
+        memo1[i] = max(memo1[i - 1], memo1[i - 2] + money[i]);
+    
+    memo2[1] = money[1];
+    for (int i = 2; i < money.size(); ++i)
+        memo2[i] = max(memo2[i - 1], memo2[i - 2] + money[i]);
+    
+    return max(memo1[money.size() - 2], memo2[money.size() - 1]);
+}
